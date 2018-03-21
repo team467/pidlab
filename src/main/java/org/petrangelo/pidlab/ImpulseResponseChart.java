5a2438a8daf42d1523e65149f9c5a19e33bd41fe
John Petrangelo <>
2018-03-20 21:26:58 -0400

    Refine and rename the first chart to be the ImpuseResponseChart

---
+++ src/main/java/org/petrangelo/pidlab/ImpulseResponseChart.java
@@ -0,0 +1,69 @@
+package org.petrangelo.pidlab;
+
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+import javax.swing.WindowConstants;
+
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.data.xy.XYDataset;
+import org.jfree.data.xy.XYSeries;
+import org.jfree.data.xy.XYSeriesCollection;
+
+public class ImpulseResponseChart extends JFrame {
+	private static final long serialVersionUID = 1L;
+
+	public ImpulseResponseChart(String title) {
+		super(title);
+
+		// Create dataset.
+		XYDataset dataset = createDataset();
+
+		// Create chart.
+		boolean wantLegend = true;
+		boolean wantTooltips = true;
+		boolean wantURLs = false;
+		JFreeChart chart = ChartFactory.createScatterPlot(
+				"Motor Simulation", "Time (sec)", "", dataset,
+				PlotOrientation.VERTICAL, wantLegend, wantTooltips, wantURLs);
+
+		// Create panel in which to display the chart.
+		ChartPanel panel = new ChartPanel(chart);
+		setContentPane(panel);
+	}
+
+	private XYDataset createDataset() {
+		MotorModel model = new MotorModel(1000, 2);
+
+		XYSeries speedSeries = new XYSeries("Motor speed");
+		XYSeries positionSeries = new XYSeries("Motor position");
+		for (int i = 0; i < 100; i++) {
+			model.step(1.0);
+			speedSeries.add(i * MotorModel.STEP_TIME_SEC, model.getSpeed());
+			positionSeries.add(i * MotorModel.STEP_TIME_SEC, model.getPosition());
+		}
+		for (int i = 100; i < 200; i++) {
+			model.step(0.0);
+			speedSeries.add(i * MotorModel.STEP_TIME_SEC, model.getSpeed());
+			positionSeries.add(i * MotorModel.STEP_TIME_SEC, model.getPosition());
+		}
+
+		XYSeriesCollection dataset = new XYSeriesCollection();
+		dataset.addSeries(speedSeries);
+		dataset.addSeries(positionSeries);
+
+		return dataset;
+	}
+
+	public static void main(String[] args) {
+		SwingUtilities.invokeLater(() -> {
+			ImpulseResponseChart example = new ImpulseResponseChart("PID Lab");
+			example.setSize(1200, 800);
+			example.setLocationRelativeTo(null);
+			example.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+			example.setVisible(true);
+		});
+	}
+}
--- src/main/java/org/petrangelo/pidlab/MotorModel.java
+++ src/main/java/org/petrangelo/pidlab/MotorModel.java
@@ -4,18 +4,23 @@ package org.petrangelo.pidlab;
  * MotorModel simulates a motor as its input voltage is changed over time.
  */
 public class MotorModel {
+	public final static double STEP_TIME_SEC = 0.020;
+
 	private final double Kv;
 	private final double T0;
 
+	// Current speed in ticks/sec.
 	private double currentSpeed = 0.0;
+
+	// Current position in ticks.
 	private double currentPosition = 0.0;
 	
 	/**
 	 * Create a MotorModel object.
 	 * 
-	 * @param Kv motor gain, or the maximum speed of the motor at full input voltage
+	 * @param Kv motor gain, or the maximum speed of the motor at full input voltage, in ticks/second
 	 * @param T0 time constant for the motor, or the time it takes for the motor to
-	 * 		  reach ~62% of the final speed at a given input voltage
+	 * 		  reach ~62% of the final speed at a given input voltage, in seconds
 	 */
 	public MotorModel(double Kv, double T0) {
 		this.Kv = Kv;
@@ -27,8 +32,8 @@ public class MotorModel {
 	 * @param Vin the input voltage during this time increment
 	 */
 	public void step(double Vin) {
-		currentSpeed += (Kv * Vin - currentSpeed) / T0;
-		currentPosition += currentSpeed;
+		currentSpeed += STEP_TIME_SEC * (Kv * Vin - currentSpeed) / T0;
+		currentPosition += STEP_TIME_SEC * currentSpeed;
 	}
 	
 	/**
